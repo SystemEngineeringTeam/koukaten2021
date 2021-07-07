28d549c47dfd0c2cf98a1b268ea4f30700440d7e
x20064Naruse <yoy11029mc@gmail.com>
2021-07-07 15:48:18 +0900

    shownum.vue をちょっといじった nuxt/store に index.js を追加

---
+++ nuxt/components/shownum.vue
@@ -0,0 +1,19 @@
+<!--人数表示コンポーネント-->
+<template>
+    <div>
+        <!--タイトル-->
+        <h3>現在の人数</h3>
+
+        <div>
+            <!--人数表示部分-->
+            <h2>
+                
+            </h2>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
