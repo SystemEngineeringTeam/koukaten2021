12ac7204ebfe97d90bfb04f2b71825079d73bea2
x20064Naruse <yoy11029mc@gmail.com>
2021-07-09 17:40:06 +0900

    最終更新時刻(仮)を追加

--- nuxt/components/shownum.vue
+++ nuxt/components/shownum.vue
@@ -2,12 +2,14 @@
 <template>
   <v-row style="height: 450px" justify="center" align-content="center">
     <v-col cols="6">
-      <v-card elecaiton="50" width="500" class="justify-center">
+      <v-card elecaiton="50" class="justify-center">
         <v-row align="center">
           <v-col cols="12">
             <v-card-text class="text-center">
+              <h3>最終更新時刻：ｎ月ｎ日ｎ時ｎ分</h3><br><br>
               <!--人数表示部分-->
               <h2>現在：</h2>
+              <h1>X</h1>
               <h1>{{ this.$store.state.tasks }}</h1>
               <h2>人</h2>
             </v-card-text>
@@ -25,10 +27,15 @@ export default {
 </script>
 
 <style scoped>
-h1 {
+h2{
   display: inline;
+  font-size: 32px;
 }
-h2 {
+h1{
   display: inline;
+  font-size: 64px;
+}
+h3{
+  font-size: 20px;
 }
 </style>
