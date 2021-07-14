abb55c6bee563ed495d08c55b3e14d3ddda41433
x20064Naruse <yoy11029mc@gmail.com>
2021-07-14 13:40:12 +0900

    menubar.vueをindex.vueに移植 menubar.vueの削除 dataの定義法の統一 tabに初期値を0に 未使用のdrawerとgroupの削除

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,21 +1,60 @@
 <template>
   <div id="app">
-      <menubar />
+    <v-card flat>
+      <v-toolbar color="blue" dark>
+        <v-toolbar-title>シス研人数出る蔵</v-toolbar-title>
+        <template v-slot:extension>
+          <v-tabs
+            color="white"
+            background-color="blue"
+            centered
+            fixed-tabs
+            v-model="tab"
+          >
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+            <v-tab v-for="item in items" :key="item.tab">
+              <v-icon>{{ item.tab }}</v-icon>
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <ShowNum />
+        </v-tab-item>
+        <v-tab-item>
+          <h1>工事中！</h1>
+        </v-tab-item>
+        <v-tab-item>
+          <h1>工事中！</h1>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </div>
 </template>
 
 <script>
-import menubar from '../components/menubar.vue'
-
+import ShowNum from '../components/ShowNum.vue'
 export default {
   components: {
-    menubar,
+    ShowNum,
   },
+  data: () => ({
+    drawer: false,
+    group: null,
+    tab: null,
+    items: [
+      { tab: 'mdi-human-greeting' },
+      { tab: 'mdi-equalizer' },
+      { tab: 'mdi-google-downasaur' },
+    ],
+  }),
 }
-
 </script>
 
 <style scoped>
-
-
+h1 {
+  color: #000;
+}
 </style>
