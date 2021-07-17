4a07777251699162faab9da2da885d9fcf53bb80
sonarAIT <sonarmikumiku@gmail.com>
2021-07-17 21:51:30 +0900

    feat: server.pyのprintのフォーマットを変更

--- python/server.py
+++ python/server.py
@@ -3,6 +3,11 @@ from wsgiref.simple_server import make_server
 
 import json
 import subprocess
+import os
+
+CAMERA_SERVER_STR="camera-server | "
+def camera_server_print(text):
+    print(CAMERA_SERVER_STR + text)
 
 def app(environ, start_response):
     command = ['python3', 'detect.py']
@@ -11,11 +16,11 @@ def app(environ, start_response):
         message = "ERROR: Failed to run detect.py."
         status = '500 INTERNAL SERVER ERROR'
         headers = [
-            ('Content-type', 'application/json; charset=utf-8'), 
+            ('Content-type', 'application/json; charset=utf-8'),
             ('Access-Control-Allow-Origin', '*')
         ]
         start_response(status, headers)
-        print(message)
+        camera_server_print(message)
         return [json.dumps({'status': status, 'message': message}).encode("utf-8")]
 
     res = int(re.sub(r"\D", "", res.stdout.decode()))
@@ -27,6 +32,7 @@ def app(environ, start_response):
     start_response(status, headers)
     return [json.dumps({'people': res}).encode("utf-8")]
 
+
 with make_server('', 8082, app) as httpd:
-    print("Serving on port 8082...")
+    camera_server_print("Serving on port 8082...")
     httpd.serve_forever()
