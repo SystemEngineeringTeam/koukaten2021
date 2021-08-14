ed72b4b2bb297f9eb5dbc2dfeb5635497651c27c
nissy-yuki <nisi20010510@gmail.com>
2021-08-15 08:26:12 +0900

    #44 change store & shownum

--- nuxt/store/index.js
+++ nuxt/store/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const state = () => ({
     //変数置き
     people: 0,
+    date: 0,
 })
 
 export const mutations = {
@@ -10,6 +11,9 @@ export const mutations = {
     setPeople(state, people) {
         state.people = people;
     },
+    setDate(state, date){
+        state.data = date;
+    }
 }
 
 export const actions = {
@@ -17,7 +21,8 @@ export const actions = {
         await axios
             .get('http://localhost:8081/people')
             .then((res) => {
-                context.commit("setPeople", res.data);//コ↑コ↓　わからん
+                context.commit("setPeople", res.data.people);//コ↑コ↓　わからん
+                context.commit("setDate", res.data.date);
             })
             .catch(() => {
                 console.log("peopelの取得が失敗しました。")
