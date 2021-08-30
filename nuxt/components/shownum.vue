8ddc128f9a6901a3c1abe8c98a136c18a308d685
x20064Naruse <yoy11029mc@gmail.com>
2021-08-30 22:25:16 +0900

    画像表示方法をスマートにした </v-content>の削除　dataの書き方の修正

--- nuxt/components/shownum.vue
+++ nuxt/components/shownum.vue
@@ -1,6 +1,11 @@
 <!--人数表示コンポーネント-->
 <template>
-  <v-row style="height: 700px" justify="center" align-content="center" class="bg">
+  <v-row
+    style="height: 700px"
+    justify="center"
+    align-content="center"
+    class="bg"
+  >
     <v-col cols="6">
       <v-card elecaiton="50" class="justify-center">
         <v-row align="center">
@@ -16,39 +21,7 @@
             <br />
             <!-- 画像表示部分 -->
             <v-row justify="center">
-              <div v-if="this.$store.state.people　=== 0">
-                <v-img v-bind:src="meter_0" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 1">
-                <v-img v-bind:src="meter_01" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 2">
-                <v-img v-bind:src="meter_02" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 3">
-                <v-img v-bind:src="meter_03" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 4">
-                <v-img v-bind:src="meter_04" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 5">
-                <v-img v-bind:src="meter_05" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 6">
-                <v-img v-bind:src="meter_06" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 7">
-                <v-img v-bind:src="meter_07" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 8">
-                <v-img v-bind:src="meter_08" max-width="250px"></v-img>
-              </div>
-              <div v-else-if="this.$store.state.people === 9">
-                <v-img v-bind:src="meter_09" max-width="250px"></v-img>
-              </div>
-              <div v-else>
-                <v-img v-bind:src="meter_10" max-width="250px"></v-img>
-              </div>
+              <v-img v-bind:src="meterImage" max-width="250px"></v-img>
             </v-row>
             <br />
           </v-col>
@@ -56,27 +29,34 @@
       </v-card>
     </v-col>
   </v-row>
-</v-content>
 </template>
 
 <script>
 export default {
   name: 'ShowNum',
-  data() {
-    return {
-      //画像用変数
-      meter_0: require('@/assets/images/meter_0.png'),
-      meter_01: require('@/assets/images/meter_01.png'),
-      meter_02: require('@/assets/images/meter_02.png'),
-      meter_03: require('@/assets/images/meter_03.png'),
-      meter_04: require('@/assets/images/meter_04.png'),
-      meter_05: require('@/assets/images/meter_05.png'),
-      meter_06: require('@/assets/images/meter_06.png'),
-      meter_07: require('@/assets/images/meter_07.png'),
-      meter_08: require('@/assets/images/meter_08.png'),
-      meter_09: require('@/assets/images/meter_09.png'),
-      meter_10: require('@/assets/images/meter_10.png'),
-    }
+  data: () => ({
+    //画像用配列
+    meterArray: [
+      require('@/assets/images/meter_0.png'),
+      require('@/assets/images/meter_01.png'),
+      require('@/assets/images/meter_02.png'),
+      require('@/assets/images/meter_03.png'),
+      require('@/assets/images/meter_04.png'),
+      require('@/assets/images/meter_05.png'),
+      require('@/assets/images/meter_06.png'),
+      require('@/assets/images/meter_07.png'),
+      require('@/assets/images/meter_08.png'),
+      require('@/assets/images/meter_09.png'),
+      require('@/assets/images/meter_10.png'),
+    ],
+  }),
+  computed: {
+    meterImage: function () {
+      if (this.$store.state.people > 10) {
+        return this.meterArray[10]
+      }
+      return this.meterArray[this.$store.state.people]
+    },
   },
 }
 </script>
@@ -97,6 +77,6 @@ h3 {
   top: 0;
   left: 0;
   background-size: cover;
-  background-image: url("~@/assets/images/sysken.png");
+  background-image: url('~@/assets/images/sysken.png');
 }
 </style>
