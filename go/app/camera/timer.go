468288ad80225453ac0311fe2629604cc7e52d6f
sonarAIT <sonarmikumiku@gmail.com>
2021-09-08 03:50:41 +0900

    fix: 大嘘コメントを修正, 関数を一部削除

--- go/app/camera/timer.go
+++ go/app/camera/timer.go
@@ -16,43 +16,47 @@ var (
 // 起動間隔は60の約数でなければならない
 const IGNITION_INTERVAL = 30
 
-// ResetTimer はリセットを呼び続ける関数
+// CameraTimer はカメラを定期的に起動する関数
 func CameraTimer() {
-	t := time.Now().In(jst)               // 現在時刻を取得
-	callCameraFunc(getNextTime(t).Sub(t)) // リセットし続けるための関数を呼ぶ
-}
+	startDateTime := time.Now().In(jst) // カメラ定期起動処理開始時の時刻を取得
 
-func getNextTime(t time.Time) time.Time {
-	minute := t.Minute()                                                    // 現在の分を取得
-	minute = ((int)(minute/IGNITION_INTERVAL)+1)*IGNITION_INTERVAL - minute // 発火させる時間までの分を計算
-
-	nextDate := t.Add(time.Duration(minute) * time.Minute) // minute後のデータを取得
-	Y, M, D := nextDate.Date()                             // 年月日を取得
-	h := nextDate.Hour()                                   // 時間を取得
-	m := nextDate.Minute()                                 // 分を取得
-
-	nextDate = time.Date(Y, M, D, h, m, 1, 0, jst) // 次の発火時間を取得(ミリ秒を切り捨て)
-	return nextDate
-}
-
-func callCameraFunc(d time.Duration) {
-	timer := time.NewTimer(d)
+	timer := time.NewTimer(getNextTime(startDateTime).Sub(startDateTime)) // 次の起動時刻にタイマーをセット
 
 	for t := range timer.C { // timer.Cはチャンネルから送られた現在時刻
-		// ここでカメラを起動する関数
 		log.Println("カメラ起動")
 
+		// 撮影時間による誤差の吸収のため，カメラの起動より先に次の起動時刻にタイマーをセット．
 		timer.Reset(getNextTime(t).Sub(t))
 
+		// カメラを起動する関数
 		people, err := StartCamera()
-		if err != nil {
-			log.Println("カメラの読み込みに失敗しました")
+		if err != nil { // カメラの起動に失敗した場合
+			// 失敗を報告しつつも処理は続行する
+			log.Println("カメラの起動に失敗しました")
 			log.Println(err)
 		} else { // テーブルにinsert
-			if db.InsertLog(people, t) != nil {
+			if db.InsertLog(people, t) != nil { // insertに失敗した場合
+				// 失敗を報告しつつも処理は続行する
 				log.Println("データベースでの追加に失敗しました")
 				log.Println(err)
 			}
 		}
 	}
+}
+
+// getNextTime は次のカメラ起動時刻を取得する関数
+func getNextTime(t time.Time) time.Time {
+	minute := t.Minute()                                                    // 現在の分を取得
+	minute = ((int)(minute/IGNITION_INTERVAL)+1)*IGNITION_INTERVAL - minute // 次の起動時刻までの分を計算
+
+	// 年月日,時間,分のみ取得し，秒,ミリ秒は切り捨てる．
+	nextDate := t.Add(time.Duration(minute) * time.Minute) // minute後の時刻を取得
+	Y, M, D := nextDate.Date()                             // 年月日を取得
+	h := nextDate.Hour()                                   // 時間を取得
+	m := nextDate.Minute()                                 // 分を取得
+
+	// 次の起動時刻を取得(秒を切り捨て)
+	// ここで，秒を1にすることによって，確実に起動時刻に乗ってからカメラを起動するように調節している．
+	nextDate = time.Date(Y, M, D, h, m, 1, 0, jst)
+	return nextDate
 }
