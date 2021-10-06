5490b3644234367555b440c276d7daba7c16487a
sonarAIT <sonarmikumiku@gmail.com>
2021-10-06 17:22:19 +0900

    feat: python tiemrを実装

--- python/server.py
+++ python/server.py
@@ -1,38 +1,20 @@
-import re
-from wsgiref.simple_server import make_server
+import schedule
+from time import sleep
 
-import json
-import subprocess
-import os
+# カメラ計測間隔
+IGNITION_INTERVAL = 2
 
 CAMERA_SERVER_STR="camera-server | "
 def camera_server_print(text):
     print(CAMERA_SERVER_STR + text)
 
-def app(environ, start_response):
-    command = ['python3', os.path.dirname(__file__) + '/detect.py']
-    res = subprocess.run(command, stdout=subprocess.PIPE)
-    if res.returncode != 0:
-        message = "ERROR: Failed to run detect.py."
-        status = '500 INTERNAL SERVER ERROR'
-        headers = [
-            ('Content-type', 'application/json; charset=utf-8'),
-            ('Access-Control-Allow-Origin', '*')
-        ]
-        start_response(status, headers)
-        camera_server_print(message)
-        return [json.dumps({'status': status, 'message': message}).encode("utf-8")]
+def camera_task():
+    print("タスク実行中")
+    
+for minute in range(0, 60, IGNITION_INTERVAL):
+    print(minute)
+    schedule.every().hours.at(f':{minute:02}').do(camera_task)
 
-    res = int(re.sub(r"\D", "", res.stdout.decode()))
-    status = '200 OK'
-    headers = [
-        ('Content-type', 'application/json; charset=utf-8'),
-        ('Access-Control-Allow-Origin', '*'),
-    ]
-    start_response(status, headers)
-    return [json.dumps({'people': res}).encode("utf-8")]
-
-
-with make_server('', 8082, app) as httpd:
-    camera_server_print("Serving on port 8082...")
-    httpd.serve_forever()
+while True:
+    schedule.run_pending()
+    sleep(1)
