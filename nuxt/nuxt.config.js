6ede3befde3a1fccb48efa90d9c26b845404bfab
x20064Naruse <yoy11029mc@gmail.com>
2021-10-08 15:39:50 +0900

    metaタグの編集

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -14,7 +14,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'og:description', property: 'og:description', content: 'シス研の人数を確認できるナイスガイ' },
+      { hid:'og:image', property:'og:image', content:'/metaimg.png'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
